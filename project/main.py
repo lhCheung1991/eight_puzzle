f0250ddb56a9024fd1e67dd2a9e9f6d153ffa0c0
Linghan Cheung <2661301@qq.com>
2015-12-05 17:50:46 +0800

    none

--- project/main.py
+++ project/main.py
@@ -4,9 +4,4 @@ from eight_puzzle import *
 
 if __name__ == "__main__":
     pass
-    # eight_puzzle('142358607', 'random')
-    # eight_puzzle('142358607', 'BFS')
-    # eight_puzzle('142358607', 'DFS')
-    # eight_puzzle('142358607', 'DFS', 25)
-    # eight_puzzle("763401852", "random")
-    eight_puzzle("763401852", "A*")
+    eight_puzzle("763401852", "BFS")
