47196f79b87185e53d7bb7d75c6fc7d9b754b40e
Linghan Cheung <2661301@qq.com>
2015-12-07 15:22:35 +0800

    none

--- project/main.py
+++ project/main.py
@@ -8,5 +8,6 @@ if __name__ == "__main__":
     # eight_puzzle("631074852", "Greedy")
     # eight_puzzle("631074852", "A*")
     # process_file("10_moves.txt", "BFS")
-    process_file("10_moves.txt", "Greedy", 1)
+    # process_file("10_moves.txt", "Greedy", 1)
+    process_file("10_moves.txt", "A*", 1)
     # process_file('10_moves.txt', 'DFS', 5)
